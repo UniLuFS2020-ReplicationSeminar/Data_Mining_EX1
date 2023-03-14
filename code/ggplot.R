98a487a566a9fb9315c72c25c381efac1930cb10
Miguel Corredera Payo <co.miguel12@gmail.com>
2023-03-14 15:38:47 +0100

    create new dataset with only the selected columns

--- code/ggplot.R
+++ code/ggplot.R
@@ -1,1 +1,24 @@
 #GGplot code for the group 
+
+library(tidyverse)
+library(haven)
+
+#loading dataset
+
+dataset <- read_dta("data/data_raw/qog_bas_cs_jan22.dta")
+
+View(dataset)
+
+#creating new dataset with the columns to work with
+
+ggdata <- dataset %>% 
+  select(
+    cname, #Country name
+    year, #Year               
+    ipu_l_sw, #Share of Women (Lower and Single Houses) 
+    fh_ipolity2, #Level of Democracy (Freedom House/Imputed Polity) 
+    arda_isgenpct, #Islam: Total (% Adherents) 
+    mad_gdppc, #Real GDP per Capita
+  )
+                 
+
